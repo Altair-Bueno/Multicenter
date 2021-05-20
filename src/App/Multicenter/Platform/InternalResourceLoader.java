3dd45702509f26e0cdd29774d5f1343d3b860816
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2021-05-20 19:46:32 +0200

    @Depricated InternalResourceLoader. Consultar información de IntelliJ

--- src/App/Multicenter/Platform/InternalResourceLoader.java
+++ src/App/Multicenter/Platform/InternalResourceLoader.java
@@ -3,25 +3,24 @@ package App.Multicenter.Platform;
 import java.net.URL;
 
 /**
- * Permite localizar elementos contenidos en el interior del JAR de forma sencilla.
- * Contiene constantes utilizadas para obtener la URL de los elementos contenidos
- * dentro del JAR
+ * Depricated: Para más información visitar <a href="https://www.jetbrains.com/help/idea/add-items-to-project.html#import-image-to-project">documentación de intelliJ</a>
  */
+@Deprecated
 public class InternalResourceLoader {
     // TODO InternalResourceLoader Constantes
 
     /**
-     * Devuelve una URL al elemento solicitado. Tenga en cuenta que este método solo
-     * sirve para localizar elementos en el interior de src, es decir, elementos
-     * empaquetados con la aplicación. Un ejemplo de URL válida sería
+     * Depricated: <a href="https://www.jetbrains.com/help/idea/add-items-to-project.html#import-image-to-project">documentación de intelliJ</a>
      *
-     * <b>App/Multicenter/Platform/Resources/ResourceExample.png</b>
      *
      * @param path Dirección del recurso en el interior del proyecto
      * @return URL al recurso
      */
+    /*
+    @Deprecated
     public static URL getResourceURL(String path){
         // TODO InternalResourceLoader getResourceURL
         return null;
     }
+     */
 }
