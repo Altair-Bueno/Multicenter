8ea64b00958d25f37f2b1501920228c8df0c9a02
CTMarin <64917117+CTMarin@users.noreply.github.com>
2021-05-20 17:58:38 +0200

    Create PersonalSpaceView.java

---
+++ src/App/Multicenter/GUI/PersonalSpaceView.java
@@ -0,0 +1,47 @@
+public class PersonalSpaceView {
+    //Atributtes
+    Header header;
+    Board board;
+    String name;
+  
+    //Constructor
+    public PersonalSpaceView(Header header, Board board, String name) {
+        this.header = header;
+        this.board = board;
+        this.name = name;
+    }
+  
+    //Methods
+    /**
+     * Devuelve el objeto Header actual del espacio personal 
+     * @return header Header
+     */
+    public Header getHeader() {
+        return header;
+    } 
+  
+    /**
+     * Devuelve el objeto Board actual del espacio personal 
+     * @return board Board
+     */
+    public Board getBoard() {
+        return board; 
+    }
+    
+    /**
+     * Modifica el objeto Header actual del espacio personal 
+     * @param newHeader Nueva cabecera para el espacio personal
+     */
+    public void setHeader(Header newHeader) {
+        header = newHeader;
+    } 
+    
+    /**
+     * Modifica el objeto Board actual del espacio personal 
+     * @param newBoard Nuevo tablón para el espacio personal
+     */
+    public void setBoard(Board newBoard) {
+        board = newBoard; 
+    }
+  
+}
