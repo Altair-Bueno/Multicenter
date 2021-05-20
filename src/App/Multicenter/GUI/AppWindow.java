06a354c10a4f4b5bb70ebc59f2d6b7cb7804f6e4
CTMarin <64917117+CTMarin@users.noreply.github.com>
2021-05-20 17:49:21 +0200

    Update AppWindow.java

--- src/App/Multicenter/GUI/AppWindow.java
+++ src/App/Multicenter/GUI/AppWindow.java
@@ -4,5 +4,26 @@ public class AppWindow {
     SideBar sb;
   
     //Constructor
-  
+    public AppWindow(PersonalSpaceView personalSpaceView, SideBar sideBar) {
+        ps = personalSpaceView;
+        sb = sideBar;
+    }
+    
+    //Methods
+    /**
+     * Cambia el espacio personal a mostrar en la vista principal de la aplicación
+     * 
+     * @param newPs Espacio personal a mostrar
+     */
+    public void changePersonalSpace(PersonalSpaceView newPs) {
+        ps = newPs;   
+    }
+    
+    /**
+     * Crea y muestra todos los elementos visuales de la aplicación
+     *
+     */
+    public void createAndShowGUI() {
+        
+    }
 }
