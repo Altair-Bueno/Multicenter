c997580385529b420b857ba7bcddb526481ed11f
fran1215 <66244249+fran1215@users.noreply.github.com>
2021-05-25 17:06:28 +0200

    📅CountdownWidget: fixed + javadoc added.

--- src/App/Multicenter/Widget/CountdownWidget.java
+++ src/App/Multicenter/Widget/CountdownWidget.java
@@ -3,36 +3,46 @@ package App.Multicenter.Widget;
 import App.Multicenter.Space.SearchedString;
 
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.temporal.Temporal;
-import java.time.temporal.TemporalAmount;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.SortedSet;
-import java.time.*;
 import java.util.TreeSet;
 
 public class CountdownWidget extends AbstractWidget{
-    String titulo;
-    int tiempo;
-    Clock c;
-    Clock fechaDeseada;
-    Instant tiempoFinal;
-    Instant tiempoActual;
-    Duration tiempoRestante;
+    private String titulo;
+    private Date fechaevento;
 
-    public CountdownWidget(int dia, int mes, int anyo, String titulo) throws ParseException { //Debe introducir el día, hora y minuto del evento deseado
+    /**
+     * Crea un widget de cuenta atrás, ajustando las variables
+     * de clases necesarias para su funcionamiento.
+     *
+     * <p><p>Aclaración para la implementación de la fecha del evento
+     * recibida como parámetro:
+     * Date tiene que crearse con new Date(Año, Mes, Dia), donde:
+     * <ul>
+     *     <li>Año = (año_deseado - 1900) &emsp; &emsp; (Ej: para 2022, pondriamos 2022 - 1900 = 122)</li>
+     *     <li>Mes = (mes_deseado - 1) &emsp; &emsp; &emsp; &ensp;(Ej: Para Junio (06), pondriamos 06-1 = 5)</li>
+     *     <li>Día = (dia_deseado) &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;(Tal cual, un número entre 1 y 31)</li>
+     * </ul>
+     *
+     * @param fechaevento La fecha del evento sobre la cual se crea
+     *                    la cuenta atrás.
+     * @param titulo      El título del evento.
+     */
+    public CountdownWidget(Date fechaevento, String titulo) {
         this.titulo = titulo;
-        //Test
+        this.fechaevento = fechaevento;
+    }
 
-        String fechaDS = dia + "/" + mes + "/" + anyo;
-        Date fechaFinal = new SimpleDateFormat("dd/MM/yyyy").parse(fechaDS);
+    public int[] check(){
+        int[] timetoevent = new int[4]; // 0 - Days, 1 - Hours, 2 - Minutes, 3 - Seconds
+        long diff = (fechaevento.getTime() - new Date().getTime()) / 1000; // new Date() = current system date
 
-        tiempoActual = c.instant();
-        tiempoFinal.adjustInto(fechaFinal.toInstant());
+        timetoevent[0] = (int) diff / (60*60*24); // Days
+        timetoevent[1] = (int) diff / (60*60); // Hours
+        timetoevent[2] = (int) diff / 60; // Minutes
+        timetoevent[3] = (int) diff; // Seconds
 
-        tiempoRestante = Duration.between(tiempoFinal, tiempoActual);
+        return timetoevent;
     }
 
     public SortedSet<SearchedString<Widget>> buscar(String cadena) {
