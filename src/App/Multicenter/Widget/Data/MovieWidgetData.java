f4b4655fcfdaa3f858588aeb123fd4ad8f368fae
fran1215 <66244249+fran1215@users.noreply.github.com>
2021-06-01 09:33:08 +0200

    🍿 MovieWidget fixed some bugs + Added error and loading placeholder

--- src/App/Multicenter/Widget/Data/MovieWidgetData.java
+++ src/App/Multicenter/Widget/Data/MovieWidgetData.java
@@ -3,5 +3,5 @@ package App.Multicenter.Widget.Data;
 import java.io.Serializable;
 
 public class MovieWidgetData extends WidgetData implements Serializable {
-    public String filmid;
+    public String filmid; //id stored
 }
