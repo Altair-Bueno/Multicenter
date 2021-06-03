2cbc30fe254d97b9f544932cd2fc92f6a72bd52d
fran1215 <66244249+fran1215@users.noreply.github.com>
2021-06-03 13:15:11 +0200

    ADDED: being able to delete widget only on Edit Mode

--- src/App/Multicenter/GUI/Board.java
+++ src/App/Multicenter/GUI/Board.java
@@ -12,7 +12,7 @@ import java.awt.event.MouseEvent;
 import java.util.List;
 
 public class Board extends JDesktopPane {
-    public static boolean addable = true;
+    boolean addable = true;
     //Attributes
     PersonalSpace personalSpace;
     int numWidgets;
@@ -28,14 +28,16 @@ public class Board extends JDesktopPane {
     public Board(PersonalSpace widgets) {
         this.personalSpace = widgets;
         List<Widget> widgetList = widgets.getWidgets();
+        EditListener.setBoard(this);
 
         for (Widget w : widgetList) {
             AbstractWidget wAux = (AbstractWidget) w;
-            wAux.setDefaultCloseOperation(JInternalFrame.DO_NOTHING_ON_CLOSE);
+            wAux.setClosable(false);
             wAux.addInternalFrameListener(new CloseWidgetListener(wAux, this));
             add(wAux);
             wAux.setVisible(true);
         }
+
         numWidgets = widgetList.size();
         setLayout(null);
 
@@ -86,6 +88,24 @@ public class Board extends JDesktopPane {
         numWidgets++;
     }
 
+    public void addClose(){
+        List<Widget> widgetList = this.personalSpace.getWidgets();
+
+        for (Widget w : widgetList) {
+            AbstractWidget wAux = (AbstractWidget) w;
+            wAux.setClosable(true);
+        }
+    }
+
+    public void remClose(){
+        List<Widget> widgetList = this.personalSpace.getWidgets();
+
+        for (Widget w : widgetList) {
+            AbstractWidget wAux = (AbstractWidget) w;
+            wAux.setClosable(false);
+        }
+    }
+
     /**
      * Elimina un widget del tablero donde se muestran
      *
@@ -112,6 +132,7 @@ public class Board extends JDesktopPane {
         public void actionPerformed(ActionEvent e) {
             NotesWidget notes = new NotesWidget(personalSpace.getCarpeta());
             notes.setVisible(true);
+            notes.setClosable(false);
             notes.setLocation(lastPos.x, lastPos.y);
             notes.setSize(250, 250);
             addWidget(notes);
@@ -124,6 +145,7 @@ public class Board extends JDesktopPane {
         public void actionPerformed(ActionEvent e) {
             ImageWidget imageWidget = new ImageWidget(personalSpace.getCarpeta());
             imageWidget.setVisible(true);
+            imageWidget.setClosable(false);
             imageWidget.setLocation(lastPos.x, lastPos.y);
             imageWidget.setResizable(true);
             imageWidget.setSize(250, 250);
@@ -137,6 +159,7 @@ public class Board extends JDesktopPane {
         public void actionPerformed(ActionEvent e) {
             MovieWidget movieWidget = new MovieWidget();
             movieWidget.setVisible(true);
+            movieWidget.setClosable(false);
             movieWidget.setLocation(lastPos.x, lastPos.y);
             movieWidget.setResizable(true);
             movieWidget.setSize(500, 350);
@@ -149,6 +172,7 @@ public class Board extends JDesktopPane {
         @Override
         public void actionPerformed(ActionEvent e) {
             YoutubeWidget ytWidget = new YoutubeWidget();
+            ytWidget.setClosable(false);
             ytWidget.setVisible(true);
             ytWidget.setLocation(lastPos.x, lastPos.y);
             ytWidget.setResizable(true);
--- src/App/Multicenter/GUI/EditListener.java
+++ src/App/Multicenter/GUI/EditListener.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 public class EditListener implements ActionListener {
     //Attributes
     SideBar sideBar;
+    static Board board;
 
     public EditListener(SideBar sideBar) {
         this.sideBar = sideBar;
@@ -25,18 +26,24 @@ public class EditListener implements ActionListener {
             sideBar.psv.get(sideBar.selected).board.personalSpace.toggleEditMode();
             // TODO colours
             if (sideBar.editMode) {
-                Board.addable = false;
+                board.addable = false;
+                board.addClose();
                 sideBar.delButton.setEnabled(false);
                 sideBar.addButton.setEnabled(false);
                 sideBar.editButton.setText("Edit: ON");
                 //sideBar.editButton.setBackground(new Color(51, 208, 1, 255));
             } else {
-                Board.addable = true;
+                board.addable = true;
+                board.remClose();
                 sideBar.delButton.setEnabled(true);
                 sideBar.addButton.setEnabled(true);
                 sideBar.editButton.setText("Edit: OFF");
                 //sideBar.editButton.setBackground(new Color(215, 0, 0,255));
             }
         }
+    }
+
+    public static void setBoard(Board b){
+        board = b;
     }
 }
