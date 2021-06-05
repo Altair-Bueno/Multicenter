6aae659e2e11d40de795ab4e20f6d92a80b91c06
CTMarin <carlomaco11@gmail.com>
2021-06-05 23:24:31 +0200

    [GUI] dracula

--- src/App/Multicenter/GUI/AppWindow.java
+++ src/App/Multicenter/GUI/AppWindow.java
@@ -90,7 +90,7 @@ public class AppWindow extends JFrame {
         appareance.add(theme);
         JMenuItem ligth = new JMenuItem("Light");
         JMenuItem dark = new JMenuItem("Dark");
-        JMenuItem dracula = new JMenuItem("Dracula");
+        JMenuItem darcula = new JMenuItem("Darcula");
         JMenuItem intellij = new JMenuItem("IntelliJ");
         ligth.addActionListener(e -> {
             Preferences.setTheme(0);
@@ -100,7 +100,7 @@ public class AppWindow extends JFrame {
             Preferences.setTheme(1);
             SwingUtilities.updateComponentTreeUI(this);
         });
-        dracula.addActionListener(e -> {
+        darcula.addActionListener(e -> {
             Preferences.setTheme(2);
             SwingUtilities.updateComponentTreeUI(this);
         });
@@ -111,7 +111,7 @@ public class AppWindow extends JFrame {
 
         theme.add(ligth);
         theme.add(dark);
-        theme.add(dracula);
+        theme.add(darcula);
         theme.add(intellij);
 
         this.setJMenuBar(menuBar);
